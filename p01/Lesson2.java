52390ba0345e5180b417d779a880cefbf574a472
Steppat, Michael <steppat@beuth-hochschule.de>
2018-05-06 22:26:14 +0200

    added doc and class diagram

--- p01/Lesson2.java
+++ p01/Lesson2.java
@@ -1,4 +1,9 @@
 package p01;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Lesson 2 - A simple bookkeeping program
  * Use of ArrayList, one to many associations, FileIO, organizing data storage 
@@ -14,16 +19,14 @@ public class Lesson2 {
 	 * 
 	 * int nr;    		// number of transaction
 	 * Date date; 		// transaction date
-	 
-	   int debit;
-	   int credit;
-	 
+	 * int debit;		// debit account number
+	 * int credit;		// credit account number
 	 * String text;		// transaction text
 	 * double amount; 	// amount of the transaction
 	 * 
 	 * Implement a constructor to set all the properties at instantiation.
 	 * Create getters to recall the values.
-	 * Overwrite the toString-method to print out the property values
+	 * Overwrite the toString-method to print out the property values in the csv format
 	 * Instantiate a couple of objects and test them by calling toString.
 	 * Make the declaration of the objects outside the method. We need them in further exercises.
 	 * Print the results on the console. 
@@ -39,18 +42,22 @@ public class Lesson2 {
 	 * exercise 2:
 	 * Create a second class with name "Account". This class contains:
 	 * 
-	 * int nr; 								// account nr
-	 * String id;							// identifier of the account
-	 * HashMap<Integer, Integer> transaction;	// list with all transactions due to the account
+	 * int nr; 							// account nr
+	 * String id;						// identifier of the account
+	 * double balance					// balance of the account
 	 * 
 	 * There also some methods we need:
 	 * 
 	 * 1. a constructor which takes nr and id
-	 * 2. the method "addTransaction" which takes a Transaction object as a parameter
-	 * 3. getters for nr, identifier, specific transaction and one to return the balance
+	 * 2. the methods "add" and "subract" to increase or decrease the balance
+	 * 3. getters for nr, identifier and one to return the balance
 	 * 
-	 * Create three Account objects  e.g. cash, sales, supplies (reference them also as members)
-	 * "Feed" them with some transactions
+	 * Create three Account objects: cash (2880), bank (2800) and supplies (2200) 
+	 * reference them also as members
+	 * "Feed" them with some transactions like the following:
+	 * A shop owner takes 300 $ from his bank account, buys goods for 200 $ and sells some
+	 * goods for 100 $. Before he took the money from the bank account there was a balance of
+	 * 1000 $. (You can set the star balance in the account using the add method.)
 	 * Choose one object and test all the getters
 	 * Test the amount of all three accounts  
 	 */
@@ -75,9 +82,22 @@ public class Lesson2 {
 	 * String text;
 	 * double amount;
 	 * 
-	 * We  also need a getter for specific transactions which takes the transaction number as a parameter.
+	 * The method adds the transaction to an Array. Use the following:
+	 * 	 ArrayList<Transaction> transaction = new ArrayList<>();
+	 * and do the addition like:
+	 *   Transaction transaction = new Transaction(nr, date, debit, credit, text, amount);
+	 *   this.transaction.add(transaction);
+	 * Further it is increasing the debit account:	
+	 *	 account.get(debit).add(amount);	
+	 * and decreasing the credit account:	
+	 *	 account.get(credit).subtract(amount);
+	 *
+	 * To store the account we declare a HashMap: 
+	 *   Map<Integer, Account> account = new HashMap<Integer, Account>();   
+	 * We  also need a getter for the transaction Array 
 	 * 
 	 * Instantiate the class, do some transactions and print out the results on the console.
+	 * Transaction
 	 *   
 	 */
 	
@@ -90,7 +110,9 @@ public class Lesson2 {
 	 * exercise 4:
 	 * Add a method to save the journal in a file. Use a hard-coded value for the file path first. We will
 	 * fix it in the next exercise. The format should be table like, columns separated by tabs and each entry 
-	 * in a new line. This format is similar to the csv file format.
+	 * in a new line. This format is similar to the csv file format. Save only the transactions. The
+	 * balances of accounts will be recalculated, when the file is loaded again. Take the toString method
+	 * from Transaction to save each line.
 	 */
 	
 	void exercise4() {
@@ -110,7 +132,13 @@ public class Lesson2 {
 	
 	/**
 	 * exercise 6:
-	 * At last allow the user to load a journal and print the entries on the console.
+	 * At last allow the user to load a journal and print the entries on the console. Add an
+	 * overloaded constructor to Transaction with following signature:
+	 *   Transaction(String csvLine)
+	 * The parameter is a line of a csv file. Use the split method to separate the columns:
+	 *   String[] split_s = csvLine.split("\t");
+	 * The calculation of the account balances will be discussed and implemented in the next unit.
+	 * So you need not to implement them yet.  
 	 */
 	
 	void exercise6() {
